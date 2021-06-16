3338bdae954d824371908824026a87ecb83204b4
Hanna <hannacelestereed@gmail.com>
2021-06-16 11:22:00 -0500

    Update loady.js

--- dist/loady.js
+++ dist/loady.js
@@ -1,5 +1,6 @@
-window.Loady = {
- 	animations: {
+var Loady = function(element, options) {
+	let Loady = this;
+	this.animations = {
 		snake: function (animate, svg) {
 			return window.requestAnimationFrame(function (timestamp) {
 				if (animate.start == undefined) animate.start = timestamp;
@@ -7,7 +8,7 @@ window.Loady = {
 
 				var percent = (animate.elapsed / (animate.duration / 2)) * 100;
 				var offset = (((100 - percent) / 100) * 250).toFixed(2);
-				svg.setAttribute("stroke-dashoffset", offset);
+				svg.setAttribute('stroke-dashoffset', offset);
 
 				if (animate.elapsed >= animate.duration) {
 					animate.elapsed = 0;
@@ -19,44 +20,63 @@ window.Loady = {
 			});
 		}
 	}
-};
-(function (elements) {
-	var colors = {
-		primary: "#34495e",
-		black: "#000000",
-		gray: "#95a5a6",
-		blue: "#007bff",
-		"lite-blue": "#71bbff",
-		purple: "#6f42c1",
-		pink: "#e83e8c",
-		red: "#dc3545",
-		orange: "#fd7e14",
-		yellow: "#ffc107",
-		green: "#28a745",
-		teal: "#20c997",
-		cyan: "#17a2b8",
-		white: "#ffffff"
-	};
+	this.colors = {
+		primary: '#34495e',
+		black: '#000000',
+		gray: '#95a5a6',
+		blue: '#007bff',
+		'lite-blue': '#71bbff',
+		purple: '#6f42c1',
+		pink: '#e83e8c',
+		red: '#dc3545',
+		orange: '#fd7e14',
+		yellow: '#ffc107',
+		green: '#28a745',
+		teal: '#20c997',
+		cyan: '#17a2b8',
+		white: '#ffffff'
+	}
+	/* detect loady attribute change */
+	this.watch = new MutationObserver(function (mutationsList, observer) {
+		let element = this;
+		for (let idx in mutationsList) {
+			var mutation = mutationsList[idx];
+			if (mutation.type == 'attributes') {
+				if (['data-animation', 'data-color', 'data-dir', 'data-size', 'data-speed'].indexOf(mutation.attributeName) <= -1 && !((mutation.attributeName == 'style' || mutation.attributeName == 'class') && (element.loadyWidth !== element.offsetWidth || element.loadyHeight !== element.offsetHeight))) return;
 
-	var loady = function (element) {
+				if ((mutation.attributeName == 'style' || mutation.attributeName == 'class') && (element.loadyWidth !== element.offsetWidth || element.loadyHeight !== element.offsetHeight)) {
+					if (element.loadyWidth !== element.offsetWidth) element.loadyWidth = element.offsetWidth;
+					if (element.loadyHeight !== element.offsetHeight) element.loadyHeight = element.offsetHeight;
+				};
+
+				Loady.watch.disconnect();
+				Loady.init(element);
+			}
+		}
+	});
+	this.init = function(element, options) {
 		var settings = {
-			size: element.getAttribute("data-size") ? element.getAttribute("data-size") : "",
-			duration: element.getAttribute("data-speed") ? element.getAttribute("data-speed") : "1",
-			animation: element.getAttribute("data-animation") ? element.getAttribute("data-animation") : "spin",
-			color: element.getAttribute("data-color") ? element.getAttribute("data-color") : "black",
-			phColor: element.getAttribute("data-placeholderColor") ? element.getAttribute("data-placeholderColor") : "rgba(0,0,0,0.1)",
-			direction: element.getAttribute("data-dir") ? element.getAttribute("data-dir") : ""
+			size: element.getAttribute('data-size') ? element.getAttribute('data-size') : '',
+			duration: element.getAttribute('data-speed') ? element.getAttribute('data-speed') : '1',
+			animation: element.getAttribute('data-animation') ? element.getAttribute('data-animation') : 'spin',
+			color: element.getAttribute('data-color') ? element.getAttribute('data-color') : 'black',
+			phColor: element.getAttribute('data-placeholderColor') ? element.getAttribute('data-placeholderColor') : 'rgba(0,0,0,0.1)',
+			direction: element.getAttribute('data-dir') ? element.getAttribute('data-dir') : ''
 		};
+
+		for (var name in options) {
+			settings[name] = options[name];
+		}
 
 		element.loadyWidth = element.offsetWidth;
 		element.loadyHeight = element.offsetHeight;
 		var transformOrigin = (element.loadyWidth / 2).toFixed(2) + 'px';
 
-		var html = "";
+		var html = '';
 		var params = {
 			origin: transformOrigin+' '+transformOrigin+' '+transformOrigin,
-			strokeWidth: settings.size == "thicc" ? 15 : settings.size == "thin" ? 5 : 10,
-			strokeColor: colors[settings.color] ? colors[settings.color] : settings.color,
+			strokeWidth: settings.size == 'thicc' ? 15 : settings.size == 'thin' ? 5 : 10,
+			strokeColor: Loady.colors[settings.color] ? Loady.colors[settings.color] : settings.color,
 			phColor: settings.phColor,
 		};
 		var animate = {
@@ -65,21 +85,21 @@ window.Loady = {
 			start: undefined,
 		};
 		switch (settings.animation) {
-			case "snake":
+			case 'snake':
 				params.radius = settings.size == "thicc" ? 40 : 45;
 				html = '<svg xmlns="http://www.w3.org/2000/svg" class="loady-placeholder" viewBox="0 0 100 100" width="100%" height="100%" stroke="'+params.phColor+'" stroke-width="'+params.strokeWidth+'" style="transform-origin: '+params.origin+'"><circle cx="50" cy="50" r="'+params.radius+'" fill="none"></circle></svg><svg xmlns="http://www.w3.org/2000/svg" class="loady-path-1" viewBox="0 0 100 100" width="100%" height="100%" stroke="'+params.strokeColor+'" stroke-width="'+params.strokeWidth+'" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="250" stroke-dashoffset="250" transform-origin="'+params.origin+'" style="transform-origin: '+params.origin+'"><circle cx="50" cy="50" r="'+params.radius+'" fill="none"></circle></svg>';
 				break;
-			case "spin":
+			case 'spin':
 				params.radius = settings.size == "thicc" ? 40 : 45;
 				html = '<svg xmlns="http://www.w3.org/2000/svg" class="loady-placeholder" viewBox="0 0 100 100" width="100%" height="100%" stroke="'+params.phColor+'" stroke-width="'+params.strokeWidth+'" style="transform-origin: '+params.origin+'"><circle cx="50" cy="50" r="'+params.radius+'" fill="none"></circle></svg><svg xmlns="http://www.w3.org/2000/svg" class="loady-path-1" viewBox="0 0 100 100" width="100%" height="100%" stroke="'+params.strokeColor+'" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="250" stroke-dashoffset="125" stroke-width="'+params.strokeWidth+'" transform-origin="'+params.origin+'" style="transform-origin: '+params.origin+'"><circle cx="50" cy="50" r="'+params.radius+'" fill="none"></circle></svg>';
 				break;
-			case "three-spin":
+			case 'three-spin':
 				params.rad1 = settings.size == "thicc" ? 40 : 45;
 				params.rad2 = settings.size == "thicc" ? 25 : 30;
 				params.rad3 = settings.size == "thicc" ? 10 : 15;
 				html = '<svg xmlns="http://www.w3.org/2000/svg" class="loady-path-1" viewBox="0 0 100 100" width="100%" height="100%" stroke="'+params.strokeColor+'" stroke-width="'+params.strokeWidth+'" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="250" stroke-dashoffset="71.5" transform-origin="'+params.origin+'" style="transform-origin: '+params.origin+'"><circle cx="50" cy="50" r="'+params.rad1+'" fill="none"></circle></svg><svg xmlns="http://www.w3.org/2000/svg" class="loady-path-2" viewBox="0 0 100 100" width="100%" height="100%" stroke="'+params.strokeColor+'" stroke-width="'+params.strokeWidth+'" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="200" stroke-dashoffset="105" style="transform-origin: '+params.origin+'"><circle cx="50" cy="50" r="'+params.rad2+'" fill="none" transform-origin="'+params.origin+'"></circle></svg><svg xmlns="http://www.w3.org/2000/svg" class="loady-path-3" viewBox="0 0 100 100" width="100%" height="100%" stroke="'+params.strokeColor+'" stroke-width="'+params.strokeWidth+'" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="100" stroke-dashoffset="75" transform-origin="'+params.origin+'" style="transform-origin: '+params.origin+'"><circle cx="50" cy="50" r="'+params.rad3+'" fill="none"></circle></svg>';
 				break;
-			case "three-dot":
+			case 'three-dot':
 				params.rad1 = settings.size == "thicc" ? 35 : 30;
 				params.rad2 = settings.size == "thicc" ? 25 : 20;
 				params.rad3 = settings.size == "thicc" ? 15 : 10;
@@ -96,49 +116,53 @@ window.Loady = {
 		}
 
 		/* set attributes */
-		element.setAttribute("data-animation", settings.animation);
-		element.setAttribute("data-color", settings.color);
-		element.setAttribute("data-dir", settings.dir);
-		element.setAttribute("data-size", settings.size);
-		element.setAttribute("data-speed", settings.duration);
+		element.setAttribute('data-animation', settings.animation);
+		element.setAttribute('data-color', settings.color);
+		element.setAttribute('data-dir', settings.dir);
+		element.setAttribute('data-size', settings.size);
+		element.setAttribute('data-speed', settings.duration);
 
 		/* detect loady attribute change */
-		const loadyObserver = new MutationObserver(function (mutationsList, observer) {
-			for (let idx in mutationsList) {
-				var mutation = mutationsList[idx];
-				if (mutation.type == "attributes") {
-					if (["data-animation", "data-color", "data-dir", "data-size", "data-speed"].indexOf(mutation.attributeName) <= -1 && !((mutation.attributeName == 'style' || mutation.attributeName == 'class') && (element.loadyWidth !== element.offsetWidth || element.loadyHeight !== element.offsetHeight))) return;
+		Loady.watch.observe(element, { attributes: true }); // start observing
+	}
 
-					if ((mutation.attributeName == 'style' || mutation.attributeName == 'class') && (element.loadyWidth !== element.offsetWidth || element.loadyHeight !== element.offsetHeight)) {
-						if (element.loadyWidth !== element.offsetWidth) element.loadyWidth = element.offsetWidth;
-						if (element.loadyHeight !== element.offsetHeight) element.loadyHeight = element.offsetHeight;
-					};
+	this.init(element, options);
+};
 
-					loadyObserver.disconnect();
-					loady(element);
-				}
-			}
-		});
-		loadyObserver.observe(element, { attributes: true }); // start observing
-	};
+if ('jQuery' in window && '$' in window)
+	(function ($) {
+		$.fn.loady = function (options) {
+			return this.each(function () {
+				var j = $(this);
+				if (j.data('loady')) j.data('loady').watch.disconnect();
 
+				var loady = new Loady(this, options);
+				j.data('loady', loady);
+
+				return loady;
+			});
+		};
+	})(jQuery);
+
+(function (elements) {
 	Array.prototype.slice.call(elements).forEach(function (element) {
-		loady(element);
+		new Loady(element);
 	});
 
 	/* detect new loady elements */
 	const bodyObserver = new MutationObserver(function (mutationsList, observer) {
 		for (let idx in mutationsList) {
+			console.log('blah');
 			var mutation = mutationsList[idx];
-			if (mutation.type == "childList") {
+			if (mutation.type == 'childList') {
 				var added = Array.prototype.slice.call(mutation.addedNodes).filter(function (o) {
-					if (o.nodeName == "#text") return;
-					return o.classList.contains("loady");
+					if (o.nodeName == '#text') return;
+					return Array.prototype.slice.call(o.classList).indexOf('loady') > -1;
 				}).forEach(function (o) {
-					loady(o);
+					new Loady(o);
 				});
 			}
 		}
 	});
 	bodyObserver.observe(document.body, { childList: true }); // start observing
-})(document.getElementsByClassName("loady"));
+})(document.getElementsByClassName('loady'));
